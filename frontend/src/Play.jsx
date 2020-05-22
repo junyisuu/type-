6665c7f9e758af183ec94e828e61ff618b1a5a6c
kwong <kirkwong33@gmail.com>
2020-05-21 20:09:12 -0700

    Added buttons to select game choice

--- frontend/src/Play.jsx
+++ frontend/src/Play.jsx
@@ -1,24 +1,31 @@
 import React, { PureComponent, Fragment } from 'react';
+import { Segment, Divider, Input, Button, Grid, Form } from 'semantic-ui-react';
 
 export default class Play extends PureComponent {
 	render() {
 		return (
-			<Segment basic textAlign='center'>
-				<Input
-					action={{ color: 'blue', content: 'Search' }}
-					icon='search'
-					iconPosition='left'
-					placeholder='Order #'
-				/>
+			<Segment placeholder>
+				<Grid centered columns={2} relaxed='very' stackable>
+					<Grid.Column>
+						<Button
+							color='teal'
+							content='Create New Room'
+							icon='add'
+							labelPosition='left'
+						/>
+					</Grid.Column>
 
-				<Divider horizontal>Or</Divider>
+					<Grid.Column verticalAlign='middle'>
+						<Input
+							action={{ color: 'blue', content: 'Join' }}
+							icon='search'
+							iconPosition='left'
+							placeholder='Room ID'
+						/>
+					</Grid.Column>
+				</Grid>
 
-				<Button
-					color='teal'
-					content='Create New Order'
-					icon='add'
-					labelPosition='left'
-				/>
+				<Divider vertical>Or</Divider>
 			</Segment>
 		);
 	}
