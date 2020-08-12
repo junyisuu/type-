a65616545989d84e44abefb0a93fcc4ede5c1331
kwong <kirkwong33@gmail.com>
2020-08-12 14:08:03 -0700

    Add screenshot gifs to About page

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,6 +1,12 @@
 import React, { PureComponent, Fragment } from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Image } from 'semantic-ui-react';
 // import './About.css';
+
+import SignUpImg from './images/screenshots/sign_up.gif';
+import CreateJoinImg from './images/screenshots/create_join.gif';
+import ReadyImg from './images/screenshots/ready.gif';
+import RaceImg from './images/screenshots/race.gif';
+import LeaderboardImg from './images/screenshots/leaderboard.gif';
 
 export default class About extends PureComponent {
 	render() {
@@ -9,42 +15,71 @@ export default class About extends PureComponent {
 				<h1>How to Play</h1>
 				<Container>
 					<Grid>
-						<Grid.Row>
-							<Grid.Column width={6}>
-								<p>1. Create an account</p>
+						<Grid.Row style={{ height: '70vh' }}>
+							<Grid.Column width={6} verticalAlign='middle' textAlign='center'>
+								<h2>Create an account</h2>
 							</Grid.Column>
-							<Grid.Column width={10}>Insert Picture</Grid.Column>
+							<Grid.Column width={10}>
+								<Image src={SignUpImg}></Image>
+							</Grid.Column>
 						</Grid.Row>
-						<Grid.Row>
-							<Grid.Column width={6}>
-								<p>2. Create a room or join an existing one</p>
+						<Grid.Row style={{ height: '70vh' }}>
+							<Grid.Column width={10}>
+								<Image src={CreateJoinImg}></Image>
 							</Grid.Column>
-							<Grid.Column width={10}>Insert Picture</Grid.Column>
+							<Grid.Column width={6} verticalAlign='middle' textAlign='center'>
+								<h2>Create a room or join one</h2>
+							</Grid.Column>
 						</Grid.Row>
-						<Grid.Row>
-							<Grid.Column width={6}>
-								<p>3. Get ready to race</p>
+						<Grid.Row style={{ height: '70vh' }}>
+							<Grid.Column width={6} verticalAlign='middle' textAlign='center'>
+								<h2>Get ready to race</h2>
 							</Grid.Column>
-							<Grid.Column width={10}>Insert Picture</Grid.Column>
+							<Grid.Column width={10}>
+								<Image src={ReadyImg}></Image>
+							</Grid.Column>
 						</Grid.Row>
-						<Grid.Row>
-							<Grid.Column width={6}>
-								<p>4. Type!</p>
+						<Grid.Row style={{ height: '70vh' }}>
+							<Grid.Column width={10}>
+								<Image src={RaceImg}></Image>
 							</Grid.Column>
-							<Grid.Column width={10}>Insert Picture</Grid.Column>
+							<Grid.Column width={6} verticalAlign='middle' textAlign='center'>
+								<h2>Type!</h2>
+							</Grid.Column>
 						</Grid.Row>
-						<Grid.Row>
-							<Grid.Column width={6}>
-								<p>5. View the race standings and statistics</p>
+						<Grid.Row style={{ height: '70vh' }}>
+							<Grid.Column width={6} verticalAlign='middle' textAlign='center'>
+								<h2>View race standings and statistics</h2>
 							</Grid.Column>
-							<Grid.Column width={10}>Insert Picture</Grid.Column>
+							<Grid.Column width={10}>
+								<Image src={LeaderboardImg}></Image>
+							</Grid.Column>
 						</Grid.Row>
 					</Grid>
 				</Container>
 				<Container className='landing_about'>
 					<h1>About</h1>
-					<p>Create using MERN stack: MongoDB, Express, React, Node</p>
-					<p>Learn more about the project: Github link </p>
+					<p>
+						Typedash is a online multiplayer type racing game created using the
+						MERN stack (MongoDB, Express, React, Node) and hosted on AWS EC2. It
+						utilizes Socket.IO for real-time race progress as well as lobby
+						management. Its features include:
+						<ul>
+							<li>User registration, email verification, and login</li>
+							<li>Online type racing against other players</li>
+							<li>Create and join private lobbies</li>
+							<li>Real-time race progress</li>
+							<li>Race statistics</li>
+							<li>Lobby and excerpt leaderboards</li>
+							<li>Thousands of excerpts to type from</li>
+						</ul>
+					</p>
+					<p>
+						Learn more about the project by visiting:&nbsp;
+						<a href='https://github.com/kshiftw/type-'>
+							<u>https://github.com/kshiftw/type-</u>
+						</a>
+					</p>
 				</Container>
 			</Fragment>
 		);
